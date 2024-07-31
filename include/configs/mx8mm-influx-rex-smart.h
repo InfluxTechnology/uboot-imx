dd38b39b8001a737e838d1642a597277b43b29cb
Andon Nikolov <anikolov@influxtechnology.com>
2024-07-31 15:49:10 +0300

    change memmory in include/configs for influx rexgen smart board

--- include/configs/mx8mm-influx-rex-smart.h
+++ include/configs/mx8mm-influx-rex-smart.h
@@ -148,7 +148,7 @@
 
 #define CFG_SYS_SDRAM_BASE              0x40000000
 #define PHYS_SDRAM                      0x40000000
-#define PHYS_SDRAM_SIZE			0x80000000 /* 2GB DDR */
+#define PHYS_SDRAM_SIZE			0x40000000 /* 1GB DDR */
 
 #define CFG_MXC_UART_BASE		UART2_BASE_ADDR
 
